18126f7bfcc24e81df9cd5a05b9cd5340455e8ae
Abdurrahman Ibrahim <abdurrahmanibrahim.dev@gmail.com>
2025-01-15 02:12:02 -0800

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,7 @@ const projects = [
     name: "Pizza Restaurant",
     github: "https://github.com/abdurrahmanibrahimahmad/Fast-react-pizza",
     liveDemo: "https://fast-react-pizza-dev.vercel.app",
-    imageSrc: "../../public/Fast-react-pizza-photo.png",
+    imageSrc: "https://th.bing.com/th/id/OIP._m42n8kn5INF_RoiaBC_BgHaDj?rs=1&pid=ImgDetMain",
     used: "ReactJS, TailwindCSS",
     description:
       "A dynamic pizza ordering website built with React and some third-party libraries, featuring real-time location tracking, an interactive menu, and API integration for seamless ordering ",
